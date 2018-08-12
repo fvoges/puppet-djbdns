ac807b0835178e652482e46feed092c554d211e3
Federico Voges <federico@puppet.com>
2018-08-12 15:26:26 +0100

    implement main class
    add puppet strings docs to main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,21 @@
-# Manages djbdns package
+# Manages djbdns
 #
-# @summary Manages djbdns package
+# @summary Manages djbdns
 #
-# @example
+# @param [String] pkg_name
+#   defines the djbdns package name
+#
+# @param [String] daemontools_svc_dir
+#   defines the daemontools service directory
+#
+# @example Basic usage
 #   include ::djbdns
+#
 class djbdns (
+  String $pkg_name            = $djbdns::params::pkg_name,
+  String $daemontools_svc_dir = $djbdns::params::daemontools_svc_dir,
 ) inherits ::djbdns::params {
+
+  contain ::djbdns::install
 
 }
